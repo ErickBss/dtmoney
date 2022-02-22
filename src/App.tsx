8f1b2ac2e4dbcbd29a6c8a88aa7e9ee5a75f0b44
ErickBss <erickbasso22@gmail.com>
2022-02-22 11:12:13 -0300

    styled_componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 5rem;
+  color: purple;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
